459e651ff11a20647c01a63f82606fcd08a87282
Sanghak Kim <k3hppk@gmail.com>
2022-06-25 14:31:41 +0900

    tailwind 셋업하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import "./index.css";
 
 function App() {
-  return <div className="App">리액트 챗앱</div>;
+  return <h1 className="text-2xl">Hello world!</h1>;
 }
 
 export default App;
